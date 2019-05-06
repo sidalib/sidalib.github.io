510ff3ce95f51da763dcdcbe70367f8079db4807
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-05-06 04:53:39 +0200

    typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,10 +22,10 @@ const IndexPage = () => (
     />
     <div style={{ textAlign: "center", paddingTop: "100px" }}>
       <p>Still working on this site... 🔨</p>
-      <h1>Hi people I'm Sid Ali and I love coding</h1>
+      <h1>Hi people I'm Sid</h1>
       <p>
-        A Frontend Engineer who loves learning and share new things with love
-        👨‍💻❤️
+        I'm a Frontend Engineer who loves learning and sharing new things with
+        love 👨‍💻❤️
       </p>
       <div style={{ maxWidth: `600px`, display: "grid", margin: "0 auto" }}>
         <Image />
