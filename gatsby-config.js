d84bcc748ae428e7f777b967b6ee9eb32cd1a07a
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-05-07 07:34:21 +0200

    add logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#6B212F`,
         theme_color: `#6B212F`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/sid-logo.png` // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
