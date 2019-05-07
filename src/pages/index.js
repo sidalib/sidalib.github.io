dc47eae411995f971ec5c8dc395ed0bf52c02c08
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-05-07 07:21:26 +0200

    prettier update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
@@ -13,21 +13,22 @@ const IndexPage = () => (
         `gatsby`,
         `application`,
         `react`,
+        'Sid Bentifraouine',
         `Sid`,
         `Ali`,
         `Bentifraouine`,
         `Lille`,
-        `France`,
+        `France`
       ]}
     />
-    <div style={{ textAlign: "center", paddingTop: "100px" }}>
+    <div style={{ textAlign: 'center', paddingTop: '100px' }}>
       <p>Still working on this site... 🔨</p>
       <h1>Hi people I'm Sid</h1>
       <p>
         I'm a Frontend Engineer who loves learning and sharing new things with
         love 👨‍💻❤️
       </p>
-      <div style={{ maxWidth: `600px`, display: "grid", margin: "0 auto" }}>
+      <div style={{ maxWidth: `600px`, display: 'grid', margin: '0 auto' }}>
         <Image />
       </div>
     </div>
