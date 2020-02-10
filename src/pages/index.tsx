34029342e3c87ad48a7284de7a5247cdff4a6c1c
Sid Bentifraouine <sidbentifraouine@gmail.com>
2020-02-10 07:11:02 +0100

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import Typist from 'react-typist'
 import { Layout, SEO } from '../components'
 
 /* TODO: 
-  1- add img alts
-  2- remove WIP
+  - media queries
+  - add img alts
+  - remove WIP
 */
 export default () => {
   return (
@@ -16,108 +17,129 @@ export default () => {
         <Container>
           <Section>
             <h1 style={{ color: 'red' }}>👷‍♂️ WORK IN PROGRESS 🔨</h1>
-            <CSSTransition classNames='fade' in={true} appear timeout={3000}>
-              <GreetingsContainer>
-                <h1>Hi,</h1>
-                <Typist
-                  className='Typist'
-                  startDelay={500}
-                  cursor={{
-                    show: true,
-                    element: ''
-                  }}
-                >
-                  I'm Sid
-                </Typist>
-                <CSSTransition
-                  classNames='fade'
-                  in={true}
-                  appear
-                  timeout={5000}
-                >
-                  <h3>Frontend Engineer</h3>
-                </CSSTransition>
-              </GreetingsContainer>
-            </CSSTransition>
-            <CSSTransition classNames='fade' in={true} appear timeout={3000}>
-              <Image
-                src={require('../images/illustrations/coding.svg')}
-                alt=''
-              />
-            </CSSTransition>
+            <div className='Hero'>
+              <CSSTransition classNames='fade' in={true} appear timeout={3000}>
+                <GreetingsContainer>
+                  <h1>Hi, I'm Sid</h1>
+                  {/* <Typist
+                    className='Typist'
+                    startDelay={500}
+                    cursor={{
+                      show: true,
+                      element: ''
+                    }}
+                  >
+                    I'm Sid
+                  </Typist> */}
+                  <CSSTransition
+                    classNames='fade'
+                    in={true}
+                    appear
+                    timeout={5000}
+                  >
+                    <h3>Frontend Engineer</h3>
+                  </CSSTransition>
+                </GreetingsContainer>
+              </CSSTransition>
+              <CSSTransition classNames='fade' in={true} appear timeout={3000}>
+                <Image
+                  src={require('../images/illustrations/coding.svg')}
+                  alt=''
+                />
+              </CSSTransition>
+            </div>
           </Section>
 
-          {/* My Job */}
-          <Section id='my-job'>
-            <SectionTitle>My Job</SectionTitle>
-            <SectionDescription>
-              <p>
-                My name is <strong>Sid Bentifraouine</strong> and I'm a
-                <strong> Frontend Engineer</strong>.
-              </p>
-              <p>
-                I<strong> Design </strong>+<strong> Code</strong>:
-                <strong> Web </strong>+<strong> Mobile </strong>
-                Applications.
-              </p>
-              <p>
-                You need help with you <strong>React</strong>,
-                <strong> React Native</strong> application ? Or
-                <strong> UX</strong> & <strong>UI</strong> advices ?
-              </p>
-              <LetsTalkLink href='mailto:sidbentifraouine@gmail.com?subject=Hi there 🖖!'>
-                Let's Talk
-              </LetsTalkLink>
-            </SectionDescription>
-          </Section>
+          <Content>
+            <Section id='intro'>
+              <SectionTitle>Who am I ?</SectionTitle>
+              <SectionDescription style={{ textAlign: 'center' }}>
+                <p>
+                  My name is <strong>Sid Bentifraouine</strong> and I'm a
+                  <strong> Frontend Engineer</strong>
+                  <br /> I<strong> Design </strong>+<strong> Code</strong>:
+                  <strong> Web </strong>+<strong> Mobile </strong>
+                  Applications.
+                </p>
+                <p>
+                  You need help with you <strong>React</strong>,
+                  <strong> React Native</strong> application ? Or
+                  <strong> UX</strong> & <strong>UI</strong> advices ?
+                </p>
+                <LetsTalkLink href='mailto:sidbentifraouine@gmail.com?subject=Hi there 🖖!'>
+                  Let's Talk
+                </LetsTalkLink>
+              </SectionDescription>
+            </Section>
 
-          <Section>
-            <SectionTitle>Aliqua id</SectionTitle>
-            <Image
-              src={require('../images/illustrations/cooking.svg')}
-              alt='Cooking an app with multiple technologies'
-            />
-            <SectionDescription>
-              Reprehenderit est elit pariatur enim dolore adipisicing incididunt
-              qui et. Elit nulla Lorem deserunt elit. Consequat pariatur dolore
-              commodo minim nulla exercitation aute aute ad enim ad. Et id dolor
-              eiusmod minim sit aliqua aute eu tempor.
-            </SectionDescription>
-          </Section>
+            <Section>
+              <SectionTitle>Aliqua id</SectionTitle>
+              <SectionDescription>
+                Reprehenderit est elit pariatur enim dolore adipisicing
+                incididunt qui et. Elit nulla Lorem deserunt elit. Consequat
+                pariatur dolore commodo minim nulla exercitation aute aute ad
+                enim ad. Et id dolor eiusmod minim sit aliqua aute eu tempor.
+              </SectionDescription>
 
-          <Section>
-            <SectionTitle>Sit aliquip</SectionTitle>
-            <SectionDescription>
-              Ipsum aliquip culpa tempor anim sunt duis reprehenderit do officia
-              cupidatat amet commodo. Adipisicing consequat est aliqua nostrud
-              non. Nostrud veniam velit cupidatat ex. Officia aliqua esse minim
-              anim. Incididunt pariatur cupidatat amet sit dolore id anim
-              deserunt sit aliqua duis adipisicing.{' '}
-            </SectionDescription>
-            <Image
-              src={require('../images/illustrations/reading.svg')}
-              alt="Low fidelity illustration of Sid while he is reading a 'React for Dummies' Book"
-            />
-            <SectionDescription>
-              Anim commodo proident veniam laboris laboris sint laborum commodo.
-              Nisi eu labore nulla eu eu nulla eiusmod nisi aliquip
-              exercitation. Est non dolore anim anim commodo mollit incididunt
-              sunt mollit.
-            </SectionDescription>
-          </Section>
+              <div className='SideBySide'>
+                <Image
+                  src={require('../images/illustrations/cooking.svg')}
+                  alt='Cooking an app with multiple technologies'
+                />
+                <SectionDescription>
+                  Reprehenderit est elit pariatur enim dolore adipisicing
+                  incididunt qui et. Elit nulla Lorem deserunt elit. Consequat
+                  pariatur dolore commodo minim nulla exercitation aute aute ad
+                  enim ad. Et id dolor eiusmod minim sit aliqua aute eu tempor.
+                </SectionDescription>
+              </div>
+            </Section>
 
-          <Section>
-            <SectionTitle>Eiusmod et qui</SectionTitle>
-            <Image
-              src={require('../images/illustrations/bjj.svg')}
-              alt='Sid is doing some BJJ'
-            />
-            <SectionDescription>
-              Consectetur nostrud magna ullamco labore in proident. Consectetur
-              dolore anim ex cupidatat amet ea ea Lorem dolor veniam labore
-              magna. Enim aliqua eiusmod proident ex duis dolore duis.
-            </SectionDescription>
-          </Section>
+            <Section>
+              <SectionTitle>Sit aliquip</SectionTitle>
+              <SectionDescription>
+                Ipsum aliquip culpa tempor anim sunt duis reprehenderit do
+                officia cupidatat amet commodo. Adipisicing consequat est aliqua
+                nostrud non. Nostrud veniam velit cupidatat ex. Officia aliqua
+                esse minim anim. Incididunt pariatur cupidatat amet sit dolore
+                id anim deserunt sit aliqua duis adipisicing.
+              </SectionDescription>
+              <div className='SideBySide'>
+                <SectionDescription>
+                  Anim commodo proident veniam laboris laboris sint laborum
+                  commodo. Nisi eu labore nulla eu eu nulla eiusmod nisi aliquip
+                  exercitation. Est non dolore anim anim commodo mollit
+                  incididunt sunt mollit.
+                </SectionDescription>
+                <Image
+                  src={require('../images/illustrations/reading.svg')}
+                  alt="Low fidelity illustration of Sid while he is reading a 'React for Dummies' Book"
+                />
+              </div>
+            </Section>
+
+            <Section>
+              <SectionTitle>Eiusmod et qui</SectionTitle>
+              <SectionDescription>
+                Consectetur nostrud magna ullamco labore in proident.
+                Consectetur dolore anim ex cupidatat amet ea ea Lorem dolor
+                veniam labore magna. Enim aliqua eiusmod proident ex duis dolore
+                duis.
+              </SectionDescription>
+              <div className='SideBySide'>
+                <Image
+                  src={require('../images/illustrations/bjj.svg')}
+                  alt='Sid is doing some BJJ'
+                />
+                <SectionDescription>
+                  Consectetur nostrud magna ullamco labore in proident.
+                  Consectetur dolore anim ex cupidatat amet ea ea Lorem dolor
+                  veniam labore magna. Enim aliqua eiusmod proident ex duis
+                  dolore duis.
+                </SectionDescription>
+              </div>
+            </Section>
+          </Content>
         </Container>
       </Layout>
     </Fragment>
@@ -129,7 +151,7 @@ const Container = styled.div`
   flex-direction: column;
 
   @media all and (min-width: 768px) {
-    padding: 0 20vw;
+    padding: 0 10vw;
   }
 
   /* Animations */
@@ -141,6 +163,12 @@ const Container = styled.div`
   .fade-appear-active {
     opacity: 1;
     transition: opacity 3000ms;
+  }
+`
+
+const Content = styled.div`
+  @media all and (min-width: 768px) {
+    padding: 0 10vw;
   }
 `
 
@@ -186,10 +214,27 @@ const Section = styled.section`
   justify-content: center;
   align-items: center;
   padding: 2rem 0.5rem;
+  .Hero {
+    @media all and (min-width: 768px) {
+      display: grid;
+      grid-template-columns: 2fr 3fr;
+      grid-column-gap: 1rem;
+      justify-items: center;
+      align-items: center;
+    }
+  }
+  .SideBySide {
+    @media all and (min-width: 768px) {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-column-gap: 1rem;
+      justify-items: center;
+    }
+  }
 `
 
 const SectionTitle = styled.h3`
-  margin: 1rem 0;
+  margin: 2rem 0;
   color: ${(props) => props.theme.primary};
 `
 
